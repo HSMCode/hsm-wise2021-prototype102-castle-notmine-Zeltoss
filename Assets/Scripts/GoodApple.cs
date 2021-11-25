c36b8a4586b716a9be6b9c793e434aa12b3b8351
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2021-11-26 00:46:15 +0100

    Update GoodApple.cs

--- Assets/Scripts/GoodApple.cs
+++ Assets/Scripts/GoodApple.cs
@@ -7,6 +7,7 @@ public class GoodApple : MonoBehaviour
 
     public float speed = 10.0f;
     private Rigidbody rb;
+    private Vector2 screenBounds;
 
 
     // Start is called before the first frame update
@@ -14,11 +15,15 @@ public class GoodApple : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector2(-speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < screenBounds.x * 10) {
+            Destroy(this.gameObject);
+        }
     }
 }
