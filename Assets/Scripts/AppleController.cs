946cce8b4fa3c0727cadcf8098c206b9d6071679
willi bücking <willi.buecking@students.hs-mainz.de>
2021-12-02 15:44:45 +0100

    make apples move

--- Assets/Scripts/AppleController.cs
+++ Assets/Scripts/AppleController.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleController : MonoBehaviour
-{
+{   
+    public Vector3 goalPosition;     //position of the bug will be used to determine launch-direction of apples 
+    public Vector3 bugPosition;
+
+    Rigidbody thisApple;            //define a variable for this apples rigidbody
+    
+    private float idlePower = 0.06f;
+    private float eatPower = 0.2f;
+    
+    private Vector3 directionIdle;
+    private Vector3 directionEat;
+    
     // Start is called before the first frame update
     void Start()
     {
+        goalPosition  = GameObject.Find("appleGoal").transform.position;        //determine position of invisible goal on startup
+        bugPosition  = GameObject.Find("Kaefer").transform.position;        //determine position of bug on startup
+        //Debug.Log(bugPosition);
+        thisApple = GetComponent<Rigidbody>();
+        directionIdle = goalPosition - transform.position;
+        directionEat = bugPosition - transform.position;
         
+        thisApple.AddForce(directionIdle * idlePower, ForceMode.Impulse);
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if (Input.GetKey(KeyCode.Space))
+        {
+            thisApple.AddForce(directionEat * eatPower, ForceMode.Force);
+        }
+        else
+            thisApple.AddForce(directionIdle * idlePower, ForceMode.Force);
         
     }
 }
--- Assets/Scripts/AppleLauncher.cs
+++ Assets/Scripts/AppleLauncher.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class AppleLauncher : MonoBehaviour
 {   
-    public Vector3 bugPosition;     //position of the bug will be used to determine launch-direction of apples
     public GameObject GoodApple;
     public GameObject BadApple;
 
     // Start is called before the first frame update
     void Start()
     {
-        bugPosition = GameObject.Find("Kaefer").transform.position;     //bugPosition is assigned the value of the position of the bug und startup
-        //Debug.Log(bugPosition);
-        InvokeRepeating("LaunchGoodApple", 1f, 1f);                     //Start regularily calling the functions that will start the apple-launching process
-        InvokeRepeating("LaunchBadApple", 10f, 2f);
+        InvokeRepeating("LaunchGoodApple", 1f, 2f);                     //Start regularily calling the functions that will start the apple-launching process
+        InvokeRepeating("LaunchBadApple", 10f, 4f);
     }
 
     void LaunchGoodApple()
@@ -31,19 +28,14 @@ public class AppleLauncher : MonoBehaviour
     {
          float timeToLaunch = Random.Range(1, 8);      //wait for a random amount of seconds before launch to keep it from getting boring
         yield return new WaitForSeconds(timeToLaunch);
-        Instantiate(GoodApple, transform.position, transform.rotation);
+        Instantiate(GoodApple, transform.position, transform.rotation);     //launch good apple
     }
 
         public IEnumerator BadAppleTimer ()
     {
-         float timeToLaunch = Random.Range(2, 10);      //wait for a random amount of seconds before launch to keep it from getting boring
+         float timeToLaunch = Random.Range(3, 10);      //wait for a random amount of seconds before launch to keep it from getting boring
         yield return new WaitForSeconds(timeToLaunch);
-        Instantiate(BadApple, transform.position, transform.rotation);
+        Instantiate(BadApple, transform.position, transform.rotation);      //launch good apple
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
