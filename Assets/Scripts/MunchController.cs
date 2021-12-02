b36be93565730d37cf8e42d16e8a7362105a5bf2
willi bücking <willi.buecking@students.hs-mainz.de>
2021-12-02 16:33:10 +0100

    tweak the system a little to be more playable, add comments to some code

--- Assets/Scripts/MunchController.cs
+++ Assets/Scripts/MunchController.cs
@@ -7,7 +7,7 @@ public class MunchController : MonoBehaviour
 {   
     public Collider foodCollider;       //create a public collider variable, the collider of "kopfunten" needs to be attached here
 
-    private int score = 0;
+    private int score = 0;      //create a variable fpr the score
 
     // Start is called before the first frame update
     void Start()
@@ -27,39 +27,43 @@ public class MunchController : MonoBehaviour
             foodCollider.enabled = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))        //if R is pressed, the scene reloads again through a reloading-method (see below)
 		{
 			ReloadingScene();
 		}
         
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)      //if an apple enters the foodCollider while it is active:
     {
-        if (other.tag == "AppleGood")
+        if (other.tag == "AppleGood")       //the script checks if the apple is good or bad, if it is good, one point is added to the score
         {
             score += 1;
             Debug.Log(score);
         }
 
-        if (other.tag == "AppleBad")
+        if (other.tag == "AppleBad")        //if it is bad, 5 points are substracted from the score
         {
-            score -= 3;
+            score -= 5;
             Debug.Log(score);
-            
-        }
-        if (score <= 0)
+
+        if (score <= 0)     //if the score drops to or below zero, the game is over and the scene restarts again via a coroutine to give the player some time to think about their mistake
+                                        //this happens inside the bad-apple-is-eaten loop to stop the game from being over before the first apple is eaten
+                                        //this happened before, because the score starts with zero
         {
             StartCoroutine("gameOver");
         }
+            
+        }
+
     }
     
-    void ReloadingScene()
+    void ReloadingScene()       //this is the method to reload the scene if the refresh-key ("R") is pressed, as mentioned above
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public IEnumerator gameOver ()
+    public IEnumerator gameOver ()      //this is the coroutine to reload the scene if the game is lost
     {
         Debug.Log("GAME OVER");
         yield return new WaitForSeconds(3);
